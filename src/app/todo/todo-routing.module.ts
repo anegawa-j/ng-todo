6324f1bc906bc0e38ae85a0159b165e6b71d9ed2
姉川純一朗 <anegawa_j@anegawajunichirounoMacBook-Pro.local>
2019-12-07 20:33:23 +0900

    impl todo component

--- src/app/todo/todo-routing.module.ts
+++ src/app/todo/todo-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'list', component: TodoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
